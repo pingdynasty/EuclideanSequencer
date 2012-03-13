d6b5cb7047f780bf4e0a85b02b5c27c539e3d2af
mars <martin@rebel-it.com>
2012-03-13 17:37:43 +0000

    updated for p1 etched board

--- device.h
+++ device.h
@@ -1,34 +1,47 @@
 
-#define SEQUENCER_STEP_A_CONTROL            0
+#define SEQUENCER_FILL_A_CONTROL            0
+#define SEQUENCER_FILL_B_CONTROL            1
+#define SEQUENCER_STEP_A_CONTROL            2
 #define SEQUENCER_STEP_B_CONTROL            3
-#define SEQUENCER_FILL_A_CONTROL            1
-#define SEQUENCER_FILL_B_CONTROL            4
-#define SEQUENCER_ROTATE_A_CONTROL          2
+#define SEQUENCER_ROTATE_A_CONTROL          4
 #define SEQUENCER_ROTATE_B_CONTROL          5
 
 #define SEQUENCER_CLOCK_DDR                 DDRD
 #define SEQUENCER_CLOCK_PORT                PORTD
 #define SEQUENCER_CLOCK_PINS                PIND
-#define SEQUENCER_CLOCK_PIN                 PORTD2         /* digital pin 2 */
+#define SEQUENCER_CLOCK_PIN                 PORTD3
 
-#define SEQUENCER_OUTPUT_DDR                DDRD
-#define SEQUENCER_OUTPUT_PORT               PORTD
-#define SEQUENCER_OUTPUT_PIN_A              PORTD6         /* digital pin 6 */
-#define SEQUENCER_OUTPUT_PIN_B              PORTD7         /* digital pin 7 */
+#define SEQUENCER_RESET_DDR                 DDRD
+#define SEQUENCER_RESET_PORT                PORTD
+#define SEQUENCER_RESET_PINS                PIND
+#define SEQUENCER_RESET_PIN                 PORTD2
+
+#define SEQUENCER_OUTPUT_DDR                DDRB
+#define SEQUENCER_OUTPUT_PORT               PORTB
+#define SEQUENCER_OUTPUT_PINS               PINB
+#define SEQUENCER_OUTPUT_PIN_A              PORTB0
+#define SEQUENCER_OUTPUT_PIN_B              PORTB1
 
 #define SEQUENCER_TRIGGER_SWITCH_DDR        DDRD
 #define SEQUENCER_TRIGGER_SWITCH_PORT       PORTD
 #define SEQUENCER_TRIGGER_SWITCH_PINS       PIND
-#define SEQUENCER_TRIGGER_SWITCH_PIN_A      PORTD4         /* digital pin 4 */
-#define SEQUENCER_TRIGGER_SWITCH_PIN_B      PORTD5         /* digital pin 5 */
+#define SEQUENCER_TRIGGER_SWITCH_PIN_A      PORTD4
+#define SEQUENCER_TRIGGER_SWITCH_PIN_B      PORTD6
 
-#define SEQUENCER_ALTERNATE_SWITCH_DDR      DDRB
-#define SEQUENCER_ALTERNATE_SWITCH_PORT     PORTB
-#define SEQUENCER_ALTERNATE_SWITCH_PINS     PINB
-#define SEQUENCER_ALTERNATE_SWITCH_PIN_A    PORTB0         /* digital pin 8 */
-#define SEQUENCER_ALTERNATE_SWITCH_PIN_B    PORTB1         /* digital pin 9 */
+#define SEQUENCER_ALTERNATE_SWITCH_DDR      DDRD
+#define SEQUENCER_ALTERNATE_SWITCH_PORT     PORTD
+#define SEQUENCER_ALTERNATE_SWITCH_PINS     PIND
+#define SEQUENCER_ALTERNATE_SWITCH_PIN_A    PORTD5
+#define SEQUENCER_ALTERNATE_SWITCH_PIN_B    PORTD7
 
 #define SEQUENCER_CHAINED_SWITCH_DDR        DDRB
 #define SEQUENCER_CHAINED_SWITCH_PORT       PORTB
 #define SEQUENCER_CHAINED_SWITCH_PINS       PINB
-#define SEQUENCER_CHAINED_SWITCH_PIN        PORTB2         /* digital pin 10 */
+#define SEQUENCER_CHAINED_SWITCH_PIN        PORTB2
+
+#define SEQUENCER_LEDS_DDR        DDRB
+#define SEQUENCER_LEDS_PORT       PORTB
+#define SEQUENCER_LEDS_PINS       PINB
+#define SEQUENCER_LED_A_PIN       PORTB4
+#define SEQUENCER_LED_B_PIN       PORTB3
+#define SEQUENCER_LED_C_PIN       PORTB5
