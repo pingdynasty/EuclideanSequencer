0a6e92e608df05c3f3fb2685f3d59152bcfbba8d
mars <martin@rebel-it.com>
2013-01-03 18:48:54 +0000

    tests of basic functions

--- EuclideanSequencerTest.cpp
+++ EuclideanSequencerTest.cpp
@@ -1,41 +1,238 @@
-/* 
-   g++ bjorklund.cpp EuclideanSequencerTest.cpp -o test
+/*
+g++ -I../RebelTechnology/Libraries/wiring -I../RebelTechnology/Libraries/avrsim -I/opt/local/include -L/opt/local/lib -o EuclideanSequencerTest -lboost_unit_test_framework  EuclideanSequencerTest.cpp ../RebelTechnology/Libraries/avrsim/avr/io.c ../RebelTechnology/Libraries/wiring/serial.c ../RebelTechnology/Libraries/avrsim/avr/interrupt.c && ./EuclideanSequencerTest
 */
-#include "bjorklund.h"
-#include <inttypes.h>
-#include <iostream>
 
-void dump(uint32_t bits, uint8_t length){
-  for(uint8_t i=0; i<length; ++i)
-    std::cout << ((bits & (1<<i)) ? 'x' : '.');
-  std::cout << std::endl;
+#define SERIAL_DEBUG
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE Test
+#include <boost/test/unit_test.hpp>
+
+#include "EuclideanSequencer.cpp"
+
+struct PinFixture {
+  PinFixture() {
+    setup();
+    PIND |= _BV(PORTD2);
+    PIND |= _BV(PORTD3);
+    PIND |= _BV(PORTD4);
+    PIND |= _BV(PORTD5);
+    PIND |= _BV(PORTD6);
+    PIND |= _BV(PORTD7);
+  }
+};
+
+bool outputIsHighA(){
+  return !(PINB & _BV(PORTB0));
 }
 
-void test(int8_t fills, int8_t steps){
-  Bjorklund algo;
-  uint32_t bits = algo.compute(steps, fills);
-  std::cout << "E(" << (int)fills << ", " << (int)steps << ")\t";
-  dump(bits, steps);
-//   for(int i=0; i<5; ++i){
-//     seq.calculate(fills, steps);
-//     seq.rol(i);
-//     dump(seq);
-//   }
+bool outputIsHighB(){
+  return !(PINB & _BV(PORTB1));
 }
 
-int main(int argc, const char* argv[] ){
-  int fills = 4;
-  int steps = 9;
-  if(argc > 1)
-    fills = atoi(argv[1]);
-  if(argc > 2)
-    steps = atoi(argv[2]);
-  test(fills, steps);
+void setChainedMode(){
+  PINB &= ~_BV(PORTB2);
 }
 
-
-void setup() {
+void setTriggerModeA(){
+  PIND |= _BV(PORTD5);
+  PIND &= ~_BV(PORTD4);
 }
 
-void loop() {
+void setTriggerModeB(){
+  PIND |= _BV(PORTD7);
+  PIND &= ~_BV(PORTD6);
+}
+
+void setToggleModeA(){
+  PIND |= _BV(PORTD4);
+  PIND &= ~_BV(PORTD5);
+}
+
+void setToggleModeB(){
+  PIND |= _BV(PORTD6);
+  PIND &= ~_BV(PORTD7);
+}
+
+void setReset(bool high = true){
+  if(high)
+    PIND &= ~_BV(PORTD2);
+  else
+    PIND |= _BV(PORTD2);
+}
+
+void setClock(bool high = true){
+  if(high)
+    PIND &= ~_BV(PORTD3);
+  else
+    PIND |= _BV(PORTD3);
+  INT1_vect();
+}
+
+void toggleClock(int times = 1){
+  for(int i=0; i<times; ++i){
+    PIND ^= _BV(PORTD3);
+    INT1_vect();
+  }
+}
+
+void pulseClock(int times = 1){
+  for(int i=0; i<times; ++i){
+    PIND &= ~_BV(PORTD3); // clock high
+    INT1_vect();
+    PIND |= _BV(PORTD3); // clock low
+    INT1_vect();
+  }
+}
+
+void setAnalogueValue(int i, float value){
+  adc_values[i] = 4095-value*4096;
+}
+
+void setRotateA(float value){
+  setAnalogueValue(4, value);
+}
+
+void setRotateB(float value){
+  setAnalogueValue(5, value);
+}
+
+void setStepA(float value){
+  setAnalogueValue(2, value);
+}
+
+void setStepB(float value){
+  setAnalogueValue(3, value);
+}
+
+void setFillA(float value){
+  setAnalogueValue(0, value);
+}
+
+void setFillB(float value){
+  setAnalogueValue(1, value);
+}
+
+struct DefaultFixture {
+  PinFixture pins;
+  DefaultFixture(){
+    setRotateA(0.5);
+    setRotateB(0.5);
+    setStepA(0.5);
+    setStepB(0.5);
+    setFillA(0.5);
+    setFillB(0.5);
+    loop();
+  }
+};
+
+int countFills(GateSequencer& seq){
+  int hits = 0;
+  for(int i=0; i<seq.length; ++i){
+    if(seq.next())
+      hits++;
+  }
+  return hits;
+}
+
+int countHighsA(int clocks){
+  int hits = 0;
+  for(int i=0; i<clocks; ++i){
+    setClock(true);
+    if(outputIsHighA())
+      hits++;
+    setClock(false);
+  }
+  return hits;
+}
+
+int countHighsB(int clocks){
+  int hits = 0;
+  for(int i=0; i<clocks; ++i){
+    setClock(true);
+    if(outputIsHighB())
+      hits++;
+    setClock(false);
+  }
+  return hits;
+}
+
+BOOST_AUTO_TEST_CASE(universeInOrder)
+{
+    BOOST_CHECK(2+2 == 4);
+}
+
+BOOST_AUTO_TEST_CASE(testDefaults){
+  PinFixture fixture;
+  loop();
+  BOOST_CHECK(!resetIsHigh());
+  BOOST_CHECK(!clockIsHigh());
+}
+
+BOOST_AUTO_TEST_CASE(testOneTriggerSequence){
+  PinFixture fixture;
+  setTriggerModeA();
+  setFillA(0.0);
+  setTriggerModeB();
+  setFillB(0.0);
+  for(float len = 0.0; len < 1.0; len += 0.01){
+    setStepA(len);
+    setStepB(len);
+    loop();
+    BOOST_CHECK_EQUAL(countFills(seqA), 1);
+    BOOST_CHECK_EQUAL(countFills(seqB), 1);
+  }  
+}
+
+BOOST_AUTO_TEST_CASE(testOneToggleSequence){
+  PinFixture fixture;
+  setToggleModeA();
+  setFillA(0.0);
+  setToggleModeB();
+  setFillB(0.0);
+  for(float len = 0.0; len < 1.0; len += 0.01){
+    setStepA(len);
+    setStepB(len);
+    loop();
+    BOOST_CHECK_EQUAL(countFills(seqA), 1);
+    BOOST_CHECK_EQUAL(countFills(seqB), 1);
+  }  
+}
+
+BOOST_AUTO_TEST_CASE(testAllTriggerSequence){
+  PinFixture fixture;
+  setTriggerModeA();
+  setFillA(1.0);
+  setTriggerModeB();
+  setFillB(1.0);
+  for(float len = 0.0; len < 1.0; len += 0.05){
+    setStepA(len);
+    setStepB(len);
+    loop();
+    int steps = seqA.length;
+    BOOST_CHECK_EQUAL(seqA.length, (int)(len*16+1));
+    BOOST_CHECK_EQUAL(seqA.length, seqB.length);
+    BOOST_CHECK_EQUAL(countFills(seqA), steps);
+    BOOST_CHECK_EQUAL(countFills(seqB), steps);
+    BOOST_CHECK_EQUAL(countHighsA(steps), steps);
+    BOOST_CHECK_EQUAL(countHighsB(steps), steps);
+  }  
+}
+
+BOOST_AUTO_TEST_CASE(testAllToggleSequence){
+  PinFixture fixture;
+  setToggleModeA();
+  setFillA(1.0);
+  setToggleModeB();
+  setFillB(1.0);
+  for(float len = 0.0; len < 1.0; len += 0.05){
+    setStepA(len);
+    setStepB(len);
+    loop();
+    int steps = seqA.length;
+    BOOST_CHECK_EQUAL(seqA.length, (int)(len*16+1));
+    BOOST_CHECK_EQUAL(seqA.length, seqB.length);
+    BOOST_CHECK_EQUAL(countHighsA(steps*2), steps);
+    BOOST_CHECK_EQUAL(countHighsB(steps*4), steps*2);
+  }  
 }
